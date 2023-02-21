a10ab14da5cc8ba6a5aae1654e2ac6c8b0a104da
cheiy <gschege@gmail.com>
2023-02-21 20:32:45 +0000

    Less than 2 - print

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -14,10 +14,7 @@ void bubble_sort(int *array, size_t size)
 	size_t index, index2;
 
 	if (size < 2)
-	{
-		print_array(array, size);
 		exit(0);
-	}
 
 	index = index2 = 0;
 	for (index = 0; index < size - 1; index++)
